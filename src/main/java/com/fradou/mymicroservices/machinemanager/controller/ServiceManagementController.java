eb10c24e8afe5ca4c11768517c93953c474f5430
Fradou <alex.fradet.1@gmail.com>
2018-11-18 14:36:53 +0100

    Update pom parent and create basic controller

---
+++ src/main/java/com/fradou/mymicroservices/machinemanager/controller/ServiceManagementController.java
@@ -0,0 +1,11 @@
+package com.fradou.mymicroservices.machinemanager.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServiceManagementController {
+
+    public void lauchService(){
+
+    }
+}
